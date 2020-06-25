324a9684fbed36889693528983bae3c2e18520ad
Derek <dereksturm263@gmail.com>
2020-06-24 22:48:06 -0700

    THis is taking longer than expected.

--- Assets/Resources/Scripts/Overworld/EnemySpawn.cs
+++ Assets/Resources/Scripts/Overworld/EnemySpawn.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
@@ -8,20 +9,34 @@ public class EnemySpawn : MonoBehaviour
     #region Enemy Slime Declaration
 
     // Standard enemies.
-    static readonly EntityData AcornSlime = new EntityData("Acorn", 0, 0, 0f, 0f, 0f, // Name & Stats.
+    static readonly EntityData Acorn = new EntityData("Acorn", 0, 0, 0f, 0f, 0f, // Name & Stats.
         new Dictionary<Type, float>() { [Types.Nature] = 1f }, // Type Affinities.
         new List<Move>() { Moves.Roll } ); // Move list.
 
-    static readonly EntityData 
+    static readonly EntityData Herb = new EntityData("Herb", 0, 0, 0f, 0f, 0f,
+        new Dictionary<Type, float>() { [Types.Nature] = 1f },
+        new List<Move>() { Moves.Roll, Moves.Slam });
+
+    static readonly EntityData Peanut = new EntityData("Peanut", 0, 0, 0f, 0f, 0f,
+        new Dictionary<Type, float>() { [Types.Nature] = 1f },
+        new List<Move>() { Moves.Slam } );
+
+    static readonly EntityData Frost = new EntityData("Frost", 0, 0, 0f, 0f, 0f,
+        new Dictionary<Type, float>() { [Types.Ice] = 1f },
+        new List<Move>() { Moves.Roll } );
+
+    static readonly EntityData Snowflake = new EntityData("Snowflake", 0, 0, 0f, 0f, 0f,
+        new Dictionary<Type, float>() { [Types.Ice] = 1f },
+        new List<Move>() { Moves.Slam });
 
     // Boss enemies.
 
     #endregion
 
-    [Tooltip("Must be equal to the number of enemies that can spawn on that island.")] private List<Transform> spawnPoints = new List<Transform>();
-
     [Tooltip("Each int represents an island 1 - 6.")] public uint islandNum;
-    private readonly Dictionary<uint, List<EntityData>> islandEnemies = new Dictionary<uint, List<EntityData>>();
+
+    private Dictionary<uint, List<Vector2>> spawnPoints = new Dictionary<uint, List<Vector2>>();
+    private Dictionary<uint, List<EntityData>> islandEnemies = new Dictionary<uint, List<EntityData>>();
 
     [SerializeField] private GameObject enemySlime = null;
 
@@ -35,15 +50,41 @@ public class EnemySpawn : MonoBehaviour
 
     private void Awake()
     {
-        // Add enemies.
-        islandEnemies[1].Add(AcornSlime);
+        // This part I hate.
+        #region Spawn Points
 
-        spawnPoints.Sort(0, spawnPoints.Count, new ShuffleComparer<Transform>());
+        spawnPoints[1].Add(new Vector2(0f, 0f));
 
-        for (int i = 0; i < spawnPoints.Count; i++)
+        #endregion
+
+        #region Enemies
+
+        islandEnemies[1].Add(Acorn);
+        islandEnemies[1].Add(Herb);
+        islandEnemies[1].Add(Peanut);
+        islandEnemies[1].Add(Frost);
+        islandEnemies[1].Add(Snowflake);
+
+        #endregion
+
+        SpawnEnemies(islandNum);
+    }
+
+    private void SwitchIsland(uint newIslandNum)
+    {
+        islandNum = newIslandNum;
+
+        SpawnEnemies(islandNum);
+    }
+
+    private void SpawnEnemies(uint island)
+    {
+        spawnPoints[island].Sort(0, spawnPoints[islandNum].Count, new ShuffleComparer<Vector2>());
+
+        for (int i = 0; i < islandEnemies[island].Count; i++)
         {
-            EntityData newEnemy = islandEnemies[islandNum][i];
-            SpawnEnemy(newEnemy, spawnPoints[i].position);
+            EntityData newEnemy = islandEnemies[island][i];
+            SpawnEnemy(newEnemy, spawnPoints[island][i]);
         }
     }
 
