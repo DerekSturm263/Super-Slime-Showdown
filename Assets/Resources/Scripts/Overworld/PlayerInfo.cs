f972db760d345941e0f0a4cf7306a98a59bbee9e
Derek <dereksturm263@gmail.com>
2020-06-26 21:46:12 -0700

    You can now tap on the title screen to go to the overworld.

---
+++ Assets/Resources/Scripts/Overworld/PlayerInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo
+{
+    public static uint goldCount;
+
+    public static Dictionary<Type, float> typeAffinities = new Dictionary<Type, float>();
+    public static List<Type> types = new List<Type>();
+
+    public static List<Move> allMoves = new List<Move>();
+    public static List<Move> activeMoves = new List<Move>();
+
+    public static List<Ability> allAbilities = new List<Ability>();
+    public static Ability activeAbility;
+
+    public static List<ShopCosmetic> allShopCosmetics;
+    public static ShopCosmetic activeShopCosmetic;
+
+    public static List<TypeCosmetic> allTypeCosmetics = new List<TypeCosmetic>();
+    public static List<TypeCosmetic> activeTypeCosmetics = new List<TypeCosmetic>();
+}
---
+++ Assets/Resources/Scripts/Title/TitleUIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+public class TitleUIManager : MonoBehaviour
+{
+    private Controls controls;
+
+    private void Awake()
+    {
+        controls = new Controls();
+
+        TouchSimulation.Enable();
+
+        controls.Player.StartMovement.performed += ctx => StartGame();
+    }
+
+    private void StartGame()
+    {
+        SceneManager.LoadScene("Overworld");
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
